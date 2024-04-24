6de3a6b70c3b0a47ab4c441c5368c21760c6226c
Pavan <pavanlakshman.a@gmail.com>
2024-04-24 21:17:28 +0530

    other-script

--- 14-current-script.sh
+++ 14-current-script.sh
@@ -5,5 +5,5 @@ COURSE="DevOps from current script"
 echo "Before calling other script, course: $COURSE"
 echo "Process Instace ID of the current shell script: $$"
 
-# ./15-other-script.sh
+./15-other-script.sh
 echo "After calling other script, course: $COURSE"
