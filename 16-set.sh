ae538a7d584a298a9527bc3397a48337fede0da8
Pavan <pavanlakshman.a@gmail.com>
2024-04-25 22:34:46 +0530

    set.sh

---
+++ 16-set.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -e
+
+USERID=$(id -u)
+if [ $USERID -ne 0 ]
+then
+    echo "Please run this script as a root user."
+    exit 1 # Manually exit if the error comes
+else 
+    echo "You are super user".
+fi
+
+dnf install mysql -y
+
+dnf install git -y
+
+
+echo "Is script proceeding?"
