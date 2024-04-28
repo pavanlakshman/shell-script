325a37cb371eaa0351ad595371a7a7a6e2622318
Pavan <pavanlakshman.a@gmail.com>
2024-04-28 20:52:14 +0530

    disk-usage

---
+++ 18-dish-usage.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+DISK_USAGE=$(df -hT | grep xfs)
+DISK_THRESHOLD=6
+
+while IFS= read -r line
+do
+    USAGE=$(df -hT | awk -F " " '{print $6F}' | cut -d "%" -f1)
+    FOLDER=$(df -hT | awk -F " " '{print $NF}')
+    if [ $USAGE -ge $DISK_USAGE ]
+    then
+        echo "$FOLDER is morethan $DISK_THRESHOLD, Current Usage: $USAGE"
+    fi
+
+done <<< $DISK_USAGE
