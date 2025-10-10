1c283db5d338cfa43154af21423e923b23efc20d
Pavan <pavanlakshman.a@gmail.com>
2025-10-10 12:05:27 +0530

    functions practice

--- 10-functions.sh
+++ 10-functions.sh
@@ -1,32 +1,69 @@
+# #!/bin/bash
+
+# USERID=$(id -u)
+# TIMESTAMP=$(date +%F-%H-%M-%S)
+# SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+# LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log # This log file is for storing the output of the file in this /tmp/$SCRIPT_NAME-$TIMESTAMP.log file
+
+# VALIDATE(){
+#    if [ $1 -ne 0 ]  # Here $1 is that we defined in the script [$?], $2 is [installing MySql]
+#    then
+#         echo "$2... FAILURE"
+#     else
+#         echo "$2... SUCCESS"
+#     fi
+# }
+
+# if [ $USERID -ne 0 ]
+# then
+#     echo "Please run this script as a root user."
+#     exit 1 # Manually exit if the error comes
+# else 
+#     echo "You are super user".
+# fi
+
+# dnf install mysql -y &>>$LOGFILE
+
+# VALIDATE $? "Installing MySql" # $1 is [$?], $2 is [installing MySql]
+
+
+# dnf install git -y &>>$LOGFILE
+
+# VALIDATE $? "Installing Git"
+
+
+
+
 #!/bin/bash
 
-USERID=$(id -u)
-TIMESTAMP=$(date +%F-%H-%M-%S)
-SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
-LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
-
+USER_ID=$(id -u)
+#TIMESTAMP=$(date +%F-%H-%M-%S)
+#SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+#LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
 VALIDATE(){
-   if [ $1 -ne 0 ]
-   then
-        echo "$2... FAILURE"
+    if [ $1 -ne 0 ]
+    then 
+        echo "$2... Failure"
     else
-        echo "$2... SUCCESS"
+        echo "$2... Success"
     fi
 }
 
-if [ $USERID -ne 0 ]
+
+
+if [ $USER_ID -ne 0 ]
 then
-    echo "Please run this script as a root user."
-    exit 1 # Manually exit if the error comes
-else 
-    echo "You are super user".
+    echo "Please run this script as root user"
+    exit 1
+else
+    echo "You are super user"
 fi
 
-dnf install mysql -y &>>$LOGFILE
+dnf install mysql -y
 
-VALIDATE $? "Installing MySql"
+VALIDATE $? "Installing Mysql"
 
 
-dnf install git -y &>>$LOGFILE
+dnf install git -y
 
 VALIDATE $? "Installing Git"
